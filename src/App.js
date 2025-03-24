424886d83b3ca6709f5b3611feeb5d77379aa91c
KimYoonChae <22400147@handong.ac.kr>
2025-03-25 01:06:45 +0900

    기능, 스타일 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from "react";
-import Main from "./components/main";
+import React, { useState } from 'react';
+import './App.css';
+import Sidebar from './components/sidebar';
+import Content from './components/content';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // 사이드바 열기/닫기 토글 함수
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
   return (
-    <div>
-      <Main />
+    <div className="App" style={{ backgroundColor: '#f0f0f0' }}>
+      <div className="w3-bar w3-top w3-black w3-large" style={{ zIndex: 4 }}>
+        <button
+          className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey"
+          onClick={toggleSidebar} // 토글 함수로 변경
+        >
+          <i className="fa fa-bars"></i> &nbsp;Menu
+        </button>
+        <span className="w3-bar-item w3-right">Logo</span>
+      </div>
+
+      <Sidebar isOpen={isOpen} closeSidebar={toggleSidebar} /> {/* 사이드바에도 토글 함수 전달 */}
+
+      {isOpen && (
+        <div
+          className="w3-overlay w3-animate-opacity"
+          onClick={toggleSidebar} // 클릭 시에도 사이드바 토글
+          style={{
+            cursor: 'pointer',
+            display: 'block', // 오버레이 항상 표시
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 2,
+         
+          }}
+          title="close side menu"
+          id="myOverlay"
+        />
+      )}
+
+      <Content />
     </div>
   );
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,60 +1,55 @@
-import React from "react";
-import avatarImage from "../images/avatar2.png";  // 이미지 경로
-import styles from "../styles/sidebar.module.css";  // 스타일 불러오기
+import React from 'react';
+import avatar2 from '../images/avatar2.png';
 
-const Sidebar = () => {
+function Sidebar({ isOpen, closeSidebar }) {
   return (
-    <div className={styles.sidebarWrapper}>
-      <img 
-        src={avatarImage}  // import한 이미지 사용
-        className={styles.avatar}
-        alt="Avatar"
-      />
-      <div className={styles.welcome}>
-        Welcome, <span style={{ fontWeight: "bold" }}>Mike</span>
-      </div>
-      <div className={styles.w3}>
-        <i className="fa fa-envelope"></i> 
-         <i className="fa fa-user"></i>
-        <i className="fa fa-cog"></i>
-      
+    <nav
+      className={`w3-sidebar w3-collapse w3-white w3-animate-left ${isOpen ? 'w3-show' : ''}`}
+      style={{
+        zIndex: 3,
+        width: '300px',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        height: '100vh',
+        display: isOpen ? 'block' : 'none', // 상태에 따라 사이드바 보이게 설정
+        
+      }}
+      id="mySidebar"
+    >
+      <br />
+      <div className="w3-container w3-row"  style={{ marginTop: '50px' }}>
+        <div className="w3-col s4">
+          <img src={avatar2} className="w3-circle w3-margin-right" style={{ width: '46px' }} />
+        </div>
+        <div className="w3-col s8 w3-bar">
+          <span>Welcome, <strong>Mike</strong></span>
+          <br />
+          <a href="#" className="w3-bar-item w3-button"><i className="fa fa-envelope"></i></a>
+          <a href="#" className="w3-bar-item w3-button"><i className="fa fa-user"></i></a>
+          <a href="#" className="w3-bar-item w3-button"><i className="fa fa-cog"></i></a>
+        </div>
       </div>
       <hr />
-      <div className={styles.dashboard}>
-        Dashboard
+      <div className="w3-container">
+        <h5>Dashboard</h5>
       </div>
-      <div className={styles.w3BarItems}>
-      <div className={styles.selectedItem}>
-          <i className="fa fa-users fa-fw"></i>&nbsp; Overview
-        </div>
-        <div className={styles.w3BarItem}>
-          <i className="fa fa-eye fa-fw"></i>&nbsp; Views
-        </div>
-        <div className={styles.w3BarItem}>
-          <i className="fa fa-users fa-fw"></i>&nbsp; Traffic
-        </div>
-        <div className={styles.w3BarItem}>
-          <i className="fa fa-bullseye fa-fw"></i>&nbsp; Geo
-        </div>
-        <div className={styles.w3BarItem}>
-          <i className="fa fa-diamond fa-fw"></i>&nbsp; Orders
-        </div>
-        <div className={styles.w3BarItem}>
-        <i class="fa fa-bell fa-fw"></i>&nbsp; News
-        </div>
-        <div className={styles.w3BarItem}>
-        <i class="fa fa-bank fa-fw"></i>&nbsp; General
-        </div>
-        <div className={styles.w3BarItem}>
-        <i class="fa fa-history fa-fw"></i>&nbsp; History
-        </div>
-        <div className={styles.w3BarItem}>
-        <i class="fa fa-cog fa-fw"></i>&nbsp; Settings
-        </div>
-        
+      <div className="w3-bar-block">
+        <a href="#" className="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onClick={closeSidebar} title="close menu">
+          <i className="fa fa-remove fa-fw"></i>&nbsp; Close Menu
+        </a>
+        <a href="#" className="w3-bar-item w3-button w3-padding w3-blue"><i className="fa fa-users fa-fw"></i>&nbsp; Overview</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-eye fa-fw"></i>&nbsp; Views</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-users fa-fw"></i>&nbsp; Traffic</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-bullseye fa-fw"></i>&nbsp; Geo</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-diamond fa-fw"></i>&nbsp; Orders</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-bell fa-fw"></i>&nbsp; News</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-bank fa-fw"></i>&nbsp; General</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-history fa-fw"></i>&nbsp; History</a>
+        <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-cog fa-fw"></i>&nbsp; Settings</a>
       </div>
-    </div>
+    </nav>
   );
-};
+}
 
 export default Sidebar;
