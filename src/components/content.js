42bf1e92192d3ece0304ef136d02ff27b7a8601b
KimYoonChae <22400147@handong.ac.kr>
2025-03-26 11:36:08 +0900

    region,feed

--- src/components/content.js
+++ src/components/content.js
@@ -1,21 +1,71 @@
-// Content.js
 import React from 'react';
 import '../styles/content.css';
-import regionImage from '../images/region.png'; // 이미지를 임포트
-import avatar1 from '../images/avatar1.png';
-import avatar2 from '../images/avatar2.png';
-import avatar3 from '../images/avatar3.png';
-import avatar5 from '../images/avatar5.png';
-import avatar6 from '../images/avatar6.png';
+import regionImage from '../images/region.png';
 
 function Content() {
   return (
-    <div className="w3-main" style={{ marginLeft: '300px', marginTop: '43px' }}>
-      <header className="container" style={{paddingTop: '22px'}}>
- <div><i className="fa fa-dashboard"></i> My Dashboard</div>
-</header>
+    <div className="main">
+      <div className="maincontainer">
+        <div><i className="fa fa-dashboard"></i> My Dashboard</div>
+      </div>
+      <div className="tabs">
+        <div className="boxx">
+          <div className="boxcon_1">
+            <div><i className="fa fa-comment"></i></div>
+            <div>
+              <h3>52</h3>
+            </div>
+            <h4>Messages</h4>
+          </div>
+        </div>
+        <div className="boxx">
+          <div className="boxcon_2">
+            <div><i className="fa fa-eye"></i></div>
+            <div>
+              <h3>99</h3>
+            </div>
+            <h4>Views</h4>
+          </div>
+        </div>
+        <div className="boxx">
+          <div className="boxcon_3">
+            <div><i className="fa fa-share-alt"></i></div>
+            <div>
+              <h3>23</h3>
+            </div>
+            <h4>Shares</h4>
+          </div>
+        </div>
+        <div className="boxx">
+          <div className="boxcon_4">
+            <div><i className="fa fa-users"></i></div>
+            <div>
+              <h3>50</h3>
+            </div>
+            <h4>Users</h4>
+          </div>
+        </div>
+      </div>
 
+      <div className="regionwrap">
+        <div className="region">
+          <div className="regiontitle">Regions</div>
+          <img src={regionImage} className="regionpng" alt="Google Regional Map" />
+        </div>
 
+        <div className="feeds">
+          <div className="feedtitle">Feeds</div>
+          <div className="feedlist">
+            <div className="feedcard"><i className="fa fa-user"></i> New record, over 90 views. <span>10 mins ago</span></div>
+            <div className="feedcard"><i className="fa fa-bell"></i> Database error. <span>15 mins ago</span></div>
+            <div className="feedcard"><i className="fa fa-users"></i> New record, over 40 users. <span>17 mins ago</span></div>
+            <div className="feedcard"><i className="fa fa-comment"></i> New comments. <span>25 mins ago</span></div>
+            <div className="feedcard"><i className="fa fa-bookmark"></i> Check transactions. <span>28 mins ago</span></div>
+            <div className="feedcard"><i className="fa fa-laptop"></i> CPU overload. <span>35 mins ago</span></div>
+            <div className="feedcard"><i className="fa fa-share-alt"></i> New shares. <span>39 mins ago</span></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
